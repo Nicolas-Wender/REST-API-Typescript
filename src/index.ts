f38b3ead2abb07be2d1da5f6ff3b094e10a63fd1
Nicolas Wender Fontes Queiroz <nicolas.w3nder@gmail.com>
2023-11-24 20:47:14 -0300

    feat: adicionando o server inicial do express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World !")
+import express from 'express'
+import { config } from 'dotenv'
+
+config({ path: './.env', encoding: 'latin1', debug: true })
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.use(express.json())
+
+app.listen(port, () =>
+  console.log(`escutando na porta http://localhost:${port}`)
+)
+
+app.get('/', (req, res) => {
+  res.send('Hello World !')
+})
